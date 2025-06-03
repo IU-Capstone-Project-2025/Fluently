83ee793a17604a2650e6550cec51963c03da1433
FunnyFoXD <tskhe_daniil@mail.ru>
2025-06-03 18:59:18 +0300

    feat: postgres and handler for preference add

--- internal/api/v1/handlers/preferences_handler.go
+++ internal/api/v1/handlers/preferences_handler.go
@@ -1,11 +1,91 @@
 package handler
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
 
-func GetUserPreferences(w http.ResponseWriter, r *http.Request) {
-    // TODO: Получить настройки пользователя
+	"fluently/go-backend/internal/repository/postgres"
+	"fluently/go-backend/internal/repository/schemas"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/google/uuid"
+)
+
+type PreferenceHandler struct {
+	Repo *postgres.PreferenceRepository
 }
 
-func UpdateUserPreferences(w http.ResponseWriter, r *http.Request) {
-    // TODO: Обновить настройки пользователя
+func (h PreferenceHandler) GetUserPreferences(w http.ResponseWriter, r *http.Request) {
+	idStr := chi.URLParam(r, "id")
+	id, err := uuid.Parse(idStr)
+	if err != nil {
+		http.Error(w, "invalid UUID", http.StatusBadRequest)
+		return
+	}
+
+	pref, err := h.Repo.GetByID(r.Context(), id)
+	if err != nil {
+		http.Error(w, "preference not found", http.StatusNotFound)
+		return
+	}
+
+	resp := schemas.PreferenceResponse{
+		ID:             pref.ID,
+		CEFRLevel:      pref.CEFRLevel,
+		Points:         pref.Points,
+		FactEveryday:   pref.FactEveryday,
+		Notifications:  pref.Notifications,
+		NotificationAt: pref.NotificationsAt,
+		WordsPerDay:    pref.WordsPerDay,
+		Goal:           pref.Goal,
+	}
+
+	json.NewEncoder(w).Encode(resp)
+}
+
+func (h *PreferenceHandler) UpdateUserPreferences(w http.ResponseWriter, r *http.Request) {
+	idStr := chi.URLParam(r, "id")
+	id, err := uuid.Parse(idStr)
+	if err != nil {
+		http.Error(w, "invalid UUID", http.StatusBadRequest)
+		return
+	}
+
+	var req schemas.CreatePreferenceRequst
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, "invalid body", http.StatusBadRequest)
+		return
+	}
+
+	pref, err := h.Repo.GetByID(r.Context(), id)
+	if err != nil {
+		http.Error(w, "preference not found", http.StatusNotFound)
+		return
+	}
+
+	pref.CEFRLevel = req.CEFRLevel
+	pref.Points = req.Points
+	pref.FactEveryday = req.FactEveryday
+	pref.Notifications = req.Notifications
+	pref.NotificationsAt = req.NotificationAt
+	pref.WordsPerDay = req.WordsPerDay
+	pref.Goal = req.Goal
+
+	if err := h.Repo.UpdatePreference(r.Context(), pref); err != nil {
+		http.Error(w, "failed to update preference", http.StatusInternalServerError)
+		return
+	}
+
+	resp := schemas.PreferenceResponse{
+		ID:             pref.ID,
+		CEFRLevel:      pref.CEFRLevel,
+		Points:         pref.Points,
+		FactEveryday:   pref.FactEveryday,
+		Notifications:  pref.Notifications,
+		NotificationAt: pref.NotificationsAt,
+		WordsPerDay:    pref.WordsPerDay,
+		Goal:           pref.Goal,
+	}
+
+	json.NewEncoder(w).Encode(resp)
 }
---
+++ internal/repository/postgres/preferences_postgres.go
@@ -0,0 +1,31 @@
+package postgres
+
+import (
+	"context"
+
+	"fluently/go-backend/internal/repository/models"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type PreferenceRepository struct {
+	db *gorm.DB
+}
+
+func NewPreferenceRepository(db *gorm.DB) *PreferenceRepository {
+	return &PreferenceRepository{db: db}
+}
+
+func (r *PreferenceRepository) GetByID(ctx context.Context, id uuid.UUID) (*models.Preference, error) {
+	var pref models.Preference
+	if err := r.db.WithContext(ctx).First(&pref, "id = ?", id).Error; err != nil {
+		return nil, err
+	}
+
+	return &pref, nil
+}
+
+func (r *PreferenceRepository) UpdatePreference(ctx context.Context, pref *models.Preference) error {
+	return r.db.WithContext(ctx).Save(pref).Error
+}
