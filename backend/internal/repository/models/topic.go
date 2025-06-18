693fc79db54fc9acaa0d3583a70d4fecefe6abac
FunnyFoXD <tskhe_daniil@mail.ru>
2025-06-18 12:41:54 +0300

    feat: add model for topic

---
+++ backend/internal/repository/models/topic.go
@@ -0,0 +1,12 @@
+package models
+
+import (
+	"github.com/google/uuid"
+)
+
+type Topic struct {
+	ID    uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
+	Title string    `gorm:"type:varchar(100);not null"`
+
+	Word *Word `gorm:"foreignKey:PrefID;constraint:OnUpdate:CASCADE,OnDelete: SET NULL"`
+}
--- backend/internal/repository/models/user.go
+++ backend/internal/repository/models/user.go
@@ -8,8 +8,8 @@ import (
 
 type User struct {
 	ID           uuid.UUID  `gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
-	GoogleID     string     `gorm:"varchar(100)"`
-	Provider     string     `gorm:"varchar(50)"`
+	GoogleID     string     `gorm:"type:varchar(100);not null"`
+	Provider     string     `gorm:"type:varchar(50)"`
 	Name         string     `gorm:"type:varchar(100);not null"`
 	Role         string     `gorm:"type:varchar(10);default:'user'"`
 	Email        string     `gorm:"type:varchar(100);uniqueIndex"`
