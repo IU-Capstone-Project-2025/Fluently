76c7a2ca6f8d33853b0e502a1bd7c1e79582e5d4
Woolfer0097 <Woolfer0097@yandex.ru>
2025-06-25 00:47:45 +0300

    feature: add base outline of telegram bot

--- telegram-bot/internal/bot/router/router.go
+++ telegram-bot/internal/bot/router/router.go
@@ -1,4 +1,12 @@
 // router.go
 package router
 
-// TODO: Implement router logic
+import (
+	"fluently/telegram-bot/internal/bot/handlers"
+
+	tele "gopkg.in/telebot.v3"
+)
+
+func Register(b *tele.Bot) {
+	b.Handle("/start", handlers.StartHandler)
+}
