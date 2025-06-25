cfc408865f32557201cdf65a3699322785b1b534
Savva Ponomarev <s.ponomarev@innopolis.university>
2025-06-25 13:12:41 +0300

    button code refactor

--- ios-app/Fluently/Fluently/Screens/LessonScreens/View/LessonVariations/PickOptionsView.swift
+++ ios-app/Fluently/Fluently/Screens/LessonScreens/View/LessonVariations/PickOptionsView.swift
@@ -22,14 +22,15 @@ struct PickOptionsView: View {
                 .foregroundStyle(.blackText)
                 .padding()
             listOfAnswers
+                .padding(.horizontal, 100)
 
             Spacer()
             
             buttonNext
+                .padding(.horizontal, 100)
 
             Spacer()
         }
-        .padding(.horizontal, 100)
     }
 
     var buttonNext: some View {
@@ -38,27 +39,14 @@ struct PickOptionsView: View {
                 onAnswerSelected(selectedAnswer)
             }
         } label: {
-            ZStack(alignment: .center) {
-                RoundedRectangle(cornerRadius: 12)
-                    .fill(.blueSecondary)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 12)
-                            .stroke(.blueAccent, lineWidth: 2)
-                    )
-                RoundedRectangle(cornerRadius: 12)
-                    .fill(.blueAccent)
-                    .frame(height: 6)
-                    .offset(y: 6)
-                    .mask(RoundedRectangle(cornerRadius: 12))
-
-                Text("Next")
-                    .foregroundStyle(.blueAccent)
-                    .padding(.vertical, 12)
-            }
-            .grayscale( selectedAnswer == nil ? 1 : 0)
-//            .frame(maxWidth: .infinity)
-            .frame(maxHeight: 60)
+            Text("Next")
+                .padding()
+                .frame(maxWidth: .infinity)
+                .modifier(ButtonViewModifier(color: .blue))
+                .grayscale( selectedAnswer == nil ? 1 : 0)
+                .frame(maxHeight: 60)
         }
+        .buttonStyle(PlainButtonStyle())
     }
 
     var listOfAnswers: some View {
@@ -74,45 +62,11 @@ struct PickOptionsView: View {
     }
 
     func sentenve() -> some View {
-        var parts = sentence.split(separator: "____")
+        let parts = sentence.split(separator: "____")
 
         if let selectedAnswer {
             return Text("\(parts[0]) _\(selectedAnswer)_ \(parts[1])")
         }
         return Text("\(parts[0]) ____ \(parts[1])")
-    }
-}
-
-struct AnswerButton: View {
-    var isSelected: Bool
-    var answer: String
-
-    var onTap: () -> Void
-
-    var body: some View {
-        Button {
-            onTap()
-        } label: {
-            ZStack(alignment: .center) {
-                RoundedRectangle(cornerRadius: 12)
-                    .fill(.orangeSecondary)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 12)
-                            .stroke(.orangePrimary, lineWidth: 2)
-                    )
-                RoundedRectangle(cornerRadius: 12)
-                    .fill(.orangePrimary)
-                    .frame(height: 6)
-                    .offset(y: 6)
-                    .mask(RoundedRectangle(cornerRadius: 12))
-
-                Text(answer)
-                    .foregroundStyle(.orangePrimary)
-                    .padding(.vertical, 12)
-            }
-            .grayscale( isSelected ? 1 : 0)
-//            .frame(maxWidth: .infinity)
-            .frame(maxHeight: 60)
-        }
     }
 }
