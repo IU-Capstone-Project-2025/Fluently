5a8f16c69f480540c4aed9ed02769172028770ef
Evgenii Bortsov <ebortsoff@yandex.ru>
2025-06-27 12:24:46 +0300

    Add Serializable annotation to ServerToken

--- android-app/app/src/main/java/ru/fluentlyapp/fluently/auth/model/ServerToken.kt
+++ android-app/app/src/main/java/ru/fluentlyapp/fluently/auth/model/ServerToken.kt
@@ -1,5 +1,8 @@
 package ru.fluentlyapp.fluently.auth.model
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class ServerToken(
     val accessToken: String,
     val refreshToken: String,
