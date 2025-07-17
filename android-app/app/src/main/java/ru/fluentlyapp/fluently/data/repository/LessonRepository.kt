c19f9458bd93150a631aca750d7f86bb090ae5fe
Evgenii Bortsov <ebortsoff@yandex.ru>
2025-07-18 02:54:04 +0300

    Fix the number of words

--- android-app/app/src/main/java/ru/fluentlyapp/fluently/data/repository/LessonRepository.kt
+++ android-app/app/src/main/java/ru/fluentlyapp/fluently/data/repository/LessonRepository.kt
@@ -71,7 +71,7 @@ interface LessonRepository {
     suspend fun finishLesson()
 }
 
-const val PREFERRED_NUMBER_OF_WORDS = 1
+const val PREFERRED_NUMBER_OF_WORDS = 10
 
 class DefaultLessonRepository @Inject constructor(
     val fluentlyApiDataSource: FluentlyApiDataSource,
